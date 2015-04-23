39f943ac72a4ddb1d294daa4adceeb7cf97336ee
Kowshar Ahmed <kowshar@joomshaper.com>
2015-04-23 16:18:43 +0600

    Added language tag to installer sql

--- admin/sql/install/mysql/install.sql
+++ admin/sql/install/mysql/install.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `#__spsimpleportfolio_items` (
   `spsimpleportfolio_tag_id` text NOT NULL,
   `url` text NOT NULL,
   `enabled` tinyint(3) NOT NULL DEFAULT '1',
+  `language` varchar(255) NOT NULL DEFAULT '*',
   `access` int(5) NOT NULL DEFAULT '1',
   `ordering` int(10) NOT NULL DEFAULT '0',
   `created_by` bigint(20) NOT NULL DEFAULT '0',
@@ -23,5 +24,6 @@ CREATE TABLE IF NOT EXISTS `#__spsimpleportfolio_tags` (
   `spsimpleportfolio_tag_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `alias` varchar(55) NOT NULL,
+  `language` varchar(255) NOT NULL DEFAULT '*',
   PRIMARY KEY (`spsimpleportfolio_tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
