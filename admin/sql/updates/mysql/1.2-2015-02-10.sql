05106df22dec598bd544eb79605b482503afdb4c
JoomShaper <support@joomshaper.com>
2015-11-23 16:29:00 +0600

    Fixed installation bug

--- admin/sql/updates/mysql/1.2-2015-02-10.sql
+++ admin/sql/updates/mysql/1.2-2015-02-10.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `#__spsimpleportfolio_items` ADD `language` VARCHAR(255) NOT NULL DEFAULT '*' AFTER `enabled`;
 ALTER TABLE `#__spsimpleportfolio_items` ADD `category_id` int(11) NOT NULL AFTER `alias`;
-ALTER TABLE `#__spsimpleportfolio_tags` ADD `language` VARCHAR(255) NOT NULL DEFAULT '*' AFTER `alias`;
