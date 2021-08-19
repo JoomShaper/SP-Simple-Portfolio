6e8cbe32d0872329a7762ac2c6f684bb42ffaa4b
Sajeeb Ahamed <sajeeb07ahamed@gmail.com>
2021-08-19 19:17:35 +0600

    :bug: fix portfolio javascript issue for safari

--- components/com_spsimpleportfolio/assets/js/spsimpleportfolio.js
+++ components/com_spsimpleportfolio/assets/js/spsimpleportfolio.js
@@ -5,35 +5,31 @@
  * @license     GNU General Public License version 2 or later.
  */
 
-jQuery(function($) {
+jQuery(window).on('load', function() {
+	var $container 	= jQuery('.sp-simpleportfolio-items');
+	var $sizer = $container.find('.shuffle__sizer');
 
-	$(window).on('load', function() {
-		var $container 	= $('.sp-simpleportfolio-items');
-		var $sizer = $container.find('.shuffle__sizer');
-
-		$container.shuffle({
-			itemSelector: '.sp-simpleportfolio-item',
-			sequentialFadeDelay: 150,
-			sizer: $sizer
-		});
-
-		// Filters
-		$('.sp-simpleportfolio-filter li a').on('click', function(event){
-			event.preventDefault();
-			var $self = $(this);
-			var $this = $(this).parent();
-
-			if($this.hasClass('active')) {
-				return;
-			}
-
-			$self.closest('ul').children().removeClass('active');
-			$self.parent().addClass('active');
-
-			var $local = $self.closest('.sp-simpleportfolio').children('.sp-simpleportfolio-items');
-			
-			$local.shuffle( 'shuffle', $this.data('group') );
-		});
+	$container.shuffle({
+		itemSelector: '.sp-simpleportfolio-item',
+		sequentialFadeDelay: 150,
+		sizer: $sizer
 	});
 
+	// Filters
+	jQuery('.sp-simpleportfolio-filter li a').on('click', function(event){
+		event.preventDefault();
+		var $self = jQuery(this);
+		var $this = jQuery(this).parent();
+
+		if($this.hasClass('active')) {
+			return;
+		}
+
+		$self.closest('ul').children().removeClass('active');
+		$self.parent().addClass('active');
+
+		var $local = $self.closest('.sp-simpleportfolio').children('.sp-simpleportfolio-items');
+		
+		$local.shuffle( 'shuffle', $this.data('group') );
+	});
 });
