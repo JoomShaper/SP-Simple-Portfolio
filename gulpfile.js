3c38122f5e727cf4b44028b63251ff2bb01c7d5a
prakash-a7x <prakash.a7x@gmail.com>
2023-12-26 15:22:31 +0600

    version name updated

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ function cleanBuild() {
 }
 
 function cleanZip() {
-	return src('./com_spsimpleportfolio_v2.0.7.zip', { read: false, allowEmpty: true }).pipe(clean());
+	return src('./com_spsimpleportfolio_v2.0.8.zip', { read: false, allowEmpty: true }).pipe(clean());
 }
 
 function copySite() {
@@ -82,7 +82,7 @@ function minify_admin_js() {
 }
 
 function makeZip() {
-	return src('./build/**/*.*').pipe(zip('com_spsimpleportfolio_v2.0.7.zip')).pipe(dest('./'));
+	return src('./build/**/*.*').pipe(zip('com_spsimpleportfolio_v2.0.8.zip')).pipe(dest('./'));
 }
 
 exports.copy = series(
