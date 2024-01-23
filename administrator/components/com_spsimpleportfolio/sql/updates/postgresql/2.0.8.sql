0ad157d831023b01acfa54f4413cf4fd43be2416
prakash-a7x <prakash.a7x@gmail.com>
2024-01-23 13:24:05 +0600

    postges update 2.0.8 sql added

---
+++ administrator/components/com_spsimpleportfolio/sql/updates/postgresql/2.0.8.sql
@@ -0,0 +1,4 @@
+-- v2.0.8
+
+ALTER TABLE "#__spsimpleportfolio_items" ALTER COLUMN "checked_out" TYPE bigint;
+ALTER TABLE "#__spsimpleportfolio_items" ALTER COLUMN "checked_out_time" TYPE timestamp without time zone;
