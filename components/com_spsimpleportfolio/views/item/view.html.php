f713be5863fba7987cc72ad38f4042c0bb27f8ee
Md Siddiqur Rahman <53170094+siddik-web@users.noreply.github.com>
2026-05-13 16:10:19 +0600

    feat: integrate seoInstance support into item view and refactor codebase to follow PSR coding standards

--- components/com_spsimpleportfolio/views/item/view.html.php
+++ components/com_spsimpleportfolio/views/item/view.html.php
@@ -9,66 +9,75 @@
 defined('_JEXEC') or die();
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView;
+use Joomla\CMS\Uri\Uri;
 
-class SpsimpleportfolioViewItem extends HtmlView {
+class SpsimpleportfolioViewItem extends HtmlView
+{
 
-	protected $item;
-	protected $params;
+    protected $item;
+    protected $params;
 
-	function display($tpl = null) {
-		// Assign data to the view
-		$this->item = $this->get('Item');
+    public function display($tpl = null)
+    {
+        // Assign data to the view
+        $this->item = $this->get('Item');
 
-		$app = Factory::getApplication();
-		$this->params = $app->getParams();
-		$menus = Factory::getApplication()->getMenu();
-		$menu = $menus->getActive();
+        $app          = Factory::getApplication();
+        $this->params = $app->getParams();
+        $menus        = Factory::getApplication()->getMenu();
+        $menu         = $menus->getActive();
 
-		if($menu) {
-			$this->params->merge($menu->getParams());
-		}
+        if ($menu) {
+            $this->params->merge($menu->getParams());
+        }
 
-		// Check for errors.
-		if (count($errors = $this->get('Errors'))) {
-			throw new Exception(implode("\n", $errors), 500);
-			return false;
-		}
+        // Check for errors.
+        if (count($errors = $this->get('Errors'))) {
+            throw new Exception(implode("\n", $errors), 500);
+            return false;
+        }
 
-		$this->_prepareDocument($this->item);
-		parent::display($tpl);
-	}
+        $this->_prepareDocument($this->item);
+        parent::display($tpl);
+    }
 
-	protected function _prepareDocument($item) {
-		$app   = Factory::getApplication();
-		$title = null;
+    protected function _prepareDocument($item)
+    {
+        $app   = Factory::getApplication();
+        $title = null;
 
-		// Because the application sets a default page title,
-		$this->params->def('page_heading', $item->title);
-		$title = $item->title;
+        // Because the application sets a default page title,
+        $this->params->def('page_heading', $item->title);
+        $title = $item->title;
 
-		if (empty($title)) {
-			$title = $app->get('sitename');
-		} elseif ($app->get('sitename_pagetitles', 0) == 1) {
-			$title = Text::sprintf('JPAGETITLE', $app->get('sitename'), $title);
-		} elseif ($app->get('sitename_pagetitles', 0) == 2) {
-			$title = Text::sprintf('JPAGETITLE', $title, $app->get('sitename'));
-		}
+        if (empty($title)) {
+            $title = $app->get('sitename');
+        } elseif ($app->get('sitename_pagetitles', 0) == 1) {
+            $title = Text::sprintf('JPAGETITLE', $app->get('sitename'), $title);
+        } elseif ($app->get('sitename_pagetitles', 0) == 2) {
+            $title = Text::sprintf('JPAGETITLE', $title, $app->get('sitename'));
+        }
 
-		$this->document->setTitle($title);
-		$this->document->addCustomTag('<meta content="'. $title .'" property="og:title" />');
-		$this->document->addCustomTag('<meta content="website" property="og:type"/>');
-		$this->document->addCustomTag('<meta content="'.Uri::current().'" property="og:url" />');
-		$this->document->addCustomTag('<meta content="'. Uri::root().$item->image.'" property="og:image" />');
+        if (isset($item->seoInstance) && $item->seoInstance) {
+            $item->seoInstance->run();
+            $this->document->setTitle($title);
+            return;
+        }
 
-		if (isset($item->description) && $item->description) {
-			$meta_desc = HTMLHelper::_('string.truncate', $item->description, 155, false, false);
-			$this->document->setDescription($meta_desc);
-			$this->document->addCustomTag('<meta content="'. $meta_desc .'" property="og:description" />');
-		}
+        $this->document->setTitle($title);
+        $this->document->addCustomTag('<meta content="' . $title . '" property="og:title" />');
+        $this->document->addCustomTag('<meta content="website" property="og:type"/>');
+        $this->document->addCustomTag('<meta content="' . Uri::current() . '" property="og:url" />');
+        $this->document->addCustomTag('<meta content="' . Uri::root() . $item->image . '" property="og:image" />');
 
-	}
+        if (isset($item->description) && $item->description) {
+            $meta_desc = HTMLHelper::_('string.truncate', $item->description, 155, false, false);
+            $this->document->setDescription($meta_desc);
+            $this->document->addCustomTag('<meta content="' . $meta_desc . '" property="og:description" />');
+        }
+
+    }
 }
--- components/com_spsimpleportfolio/views/items/view.html.php
+++ components/com_spsimpleportfolio/views/items/view.html.php
@@ -1,91 +1,94 @@
 <?php
 
 /**
-* @package     SP Simple Portfolio
-*
-* @copyright   Copyright (C) 2010 - 2025 JoomShaper. All rights reserved.
-* @license     GNU General Public License version 2 or later.
-*/
+ * @package     SP Simple Portfolio
+ *
+ * @copyright   Copyright (C) 2010 - 2025 JoomShaper. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
 
 defined('_JEXEC') or die();
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Log\Log;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
 use Joomla\CMS\MVC\View\HtmlView;
 
-class SpsimpleportfolioViewItems extends HtmlView {
+class SpsimpleportfolioViewItems extends HtmlView
+{
 
-	protected $items;
-	protected $params;
-	protected $layout_type;
+    protected $items;
+    protected $params;
+    protected $layout_type;
 
-	function display($tpl = null) {
-		// Assign data to the view
-		$model = $this->getModel();
-		$this->items = $this->get('Items');
-		$this->pagination	= $this->get('Pagination');
-		$this->tagList = $model->getTagList($this->items);
+    public function display($tpl = null)
+    {
+        // Assign data to the view
+        $model            = $this->getModel();
+        $this->items      = $this->get('Items');
+        $this->pagination = $this->get('Pagination');
+        $this->tagList    = $model->getTagList($this->items);
 
-		$app = Factory::getApplication();
-		$this->params = $app->getParams();
-		$menus = Factory::getApplication()->getMenu();
-		$menu = $menus->getActive();
+        $app          = Factory::getApplication();
+        $this->params = $app->getParams();
+        $menus        = Factory::getApplication()->getMenu();
+        $menu         = $menus->getActive();
 
-		if($menu) {
-			$this->params->merge($menu->getParams());
-		}
+        if ($menu) {
+            $this->params->merge($menu->getParams());
+        }
 
-		foreach ($this->items as $this->item) {
-			// if thumb uploaded for listing
-			$this->item->thumb = ( isset($this->item->thumbnail) && $this->item->thumbnail ) ? $this->item->thumbnail : $this->item->thumb;
-		}
+        foreach ($this->items as $this->item) {
+            // if thumb uploaded for listing
+            $this->item->thumb = (isset($this->item->thumbnail) && $this->item->thumbnail) ? $this->item->thumbnail : $this->item->thumb;
+        }
 
-		$this->layout_type = str_replace('_', '-', $this->params->get('layout_type', 'default'));
-		// Check for errors.
-		if (count($errors = $this->get('Errors'))) {
-			Log::add(implode('<br />', $errors), Log::WARNING, 'jerror');
-			return false;
-		}
+        $this->layout_type = str_replace('_', '-', $this->params->get('layout_type', 'default'));
+        // Check for errors.
+        if (count($errors = $this->get('Errors'))) {
+            Log::add(implode('<br />', $errors), Log::WARNING, 'jerror');
+            return false;
+        }
 
-		$this->_prepareDocument();
-		parent::display($tpl);
-	}
+        $this->_prepareDocument();
+        parent::display($tpl);
+    }
 
-	protected function _prepareDocument() {
-		$app   = Factory::getApplication();
-		$menus = $app->getMenu();
-		$title = null;
+    protected function _prepareDocument()
+    {
+        $app   = Factory::getApplication();
+        $menus = $app->getMenu();
+        $title = null;
 
-		$menu = $menus->getActive();
-		if ($menu) {
-			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
-		} else {
-			$this->params->def('page_heading', Text::_('COM_SPSIMPLEPORTFOLIO_DEFAULT_PAGE_TITLE'));
-		}
+        $menu = $menus->getActive();
+        if ($menu) {
+            $this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+        } else {
+            $this->params->def('page_heading', Text::_('COM_SPSIMPLEPORTFOLIO_DEFAULT_PAGE_TITLE'));
+        }
 
-		$title = $this->params->get('page_title', '');
+        $title = $this->params->get('page_title', '');
 
-		if (empty($title)) {
-			$title = $app->get('sitename');
-		} elseif ($app->get('sitename_pagetitles', 0) == 1) {
-			$title = Text::sprintf('JPAGETITLE', $app->get('sitename'), $title);
-		} elseif ($app->get('sitename_pagetitles', 0) == 2) {
-			$title = Text::sprintf('JPAGETITLE', $title, $app->get('sitename'));
-		}
+        if (empty($title)) {
+            $title = $app->get('sitename');
+        } elseif ($app->get('sitename_pagetitles', 0) == 1) {
+            $title = Text::sprintf('JPAGETITLE', $app->get('sitename'), $title);
+        } elseif ($app->get('sitename_pagetitles', 0) == 2) {
+            $title = Text::sprintf('JPAGETITLE', $title, $app->get('sitename'));
+        }
 
-		$this->document->setTitle($title);
+        $this->document->setTitle($title);
 
-		if ($this->params->get('menu-meta_description')) {
-			$this->document->setDescription($this->params->get('menu-meta_description'));
-		}
+        if ($this->params->get('menu-meta_description')) {
+            $this->document->setDescription($this->params->get('menu-meta_description'));
+        }
 
-		if ($this->params->get('menu-meta_keywords')) {
-			$this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
-		}
+        if ($this->params->get('menu-meta_keywords')) {
+            $this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
+        }
 
-		if ($this->params->get('robots')) {
-			$this->document->setMetadata('robots', $this->params->get('robots'));
-		}
-	}
+        if ($this->params->get('robots')) {
+            $this->document->setMetadata('robots', $this->params->get('robots'));
+        }
+    }
 }
